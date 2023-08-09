e9d1a3ae081d464f73f2363a700981ac9cfbc7a0
Mason McVicker <mmcvicker@crimson.ua.edu>
2023-08-09 15:50:11 -0500

    Change Dataset location to /dataset for docker use

--- datasets/SemanticKitti.py
+++ datasets/SemanticKitti.py
@@ -62,7 +62,7 @@ class SemanticKittiDataset(PointCloudDataset):
         ##########################
 
         # Dataset folder
-        self.path = '../../Data/SemanticKitti'
+        self.path = '/dataset'
 
         # Type of task conducted on this dataset
         self.dataset_task = 'slam_segmentation'
