086decdc04991ed35ca0870659f98741798e6da5
Raphaël Delhome <raphael.delhome@oslandia.com>
2024-02-05 20:33:06 +0100

    test: read and write ply

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def fixture_path():
+    yield Path(__file__).parent / "fixtures"
+
+
+@pytest.fixture
+def ply_array():
+    yield np.array(
+        [[0, 10, 200], [2, 30, 400], [7, 10, 600], [5, 60, 200]], dtype=np.float64
+    )
